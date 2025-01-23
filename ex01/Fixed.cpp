8ff883661f6f77628c62d87d5a4ef547c5c2fb14
punchlemon <tsukiwahigashini0410@gmail.com>
2025-01-23 22:52:13 +0900

    init ex01

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Fixed.hpp"
+
+int Fixed::getRawBits(void) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return _bits;
+}
+
+void Fixed::setRawBits(int bits) {
+    this->_bits = bits;
+    std::cout << "setRawBits member function called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other) {
+        this->_bits = other.getRawBits();
+    }
+    return *this;
+}
+
+Fixed::Fixed(const Fixed& other) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed::Fixed(void) : _bits(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed(void) {
+    std::cout << "Destructor called" << std::endl;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,17 @@
+#ifndef __FIXED_HPP__
+#define __FIXED_HPP__
+
+class Fixed {
+    public:
+        Fixed();
+        Fixed(const Fixed& other);
+        Fixed& operator=(const Fixed& other);
+        ~Fixed();
+        int getRawBits() const;
+        void setRawBits(int bits);
+    private:
+        int _bits;
+        static const int fractionalBits = 8;
+};
+
+#endif /* __FIXED_HPP__ */
