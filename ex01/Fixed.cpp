e3dc13e4eaf639ca76eebb503bc050655382c324
punchlemon <tsukiwahigashini0410@gmail.com>
2025-01-24 15:27:01 +0900

    update ex01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
+#include <cmath>
 #include "Fixed.hpp"
 
-int Fixed::getRawBits(void) const {
-    std::cout << "getRawBits member function called" << std::endl;
-    return _bits;
+Fixed::Fixed(void) : _bits(0) {
+    std::cout << "Default constructor called" << std::endl;
 }
 
-void Fixed::setRawBits(int bits) {
-    this->_bits = bits;
-    std::cout << "setRawBits member function called" << std::endl;
+Fixed::Fixed(const int intValue) {
+    std::cout << "Int constructor called" << std::endl;
+    this->setRawBits(intValue << _numOfFractionalBits);
+}
+
+Fixed::Fixed(const float floatValue) {
+    std::cout << "Float constructor called" << std::endl;
+    float shiftedFloatValue = roundf(floatValue * (1 << _numOfFractionalBits));
+    this->setRawBits(shiftedFloatValue);
+}
+
+Fixed::Fixed(const Fixed& other) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
 }
 
 Fixed& Fixed::operator=(const Fixed& other) {
@@ -19,15 +30,27 @@ Fixed& Fixed::operator=(const Fixed& other) {
     return *this;
 }
 
-Fixed::Fixed(const Fixed& other) {
-    std::cout << "Copy constructor called" << std::endl;
-    *this = other;
-}
-
-Fixed::Fixed(void) : _bits(0) {
-    std::cout << "Default constructor called" << std::endl;
-}
-
 Fixed::~Fixed(void) {
     std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const {
+    return _bits;
+}
+
+void Fixed::setRawBits(int bits) {
+    this->_bits = bits;
+}
+
+float Fixed::toFloat() const {
+    return static_cast<float>(this->getRawBits()) / (1 << _numOfFractionalBits);
+}
+
+int Fixed::toInt() const {
+    return this->getRawBits() >> _numOfFractionalBits;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
+    os << fixed.toFloat();
+    return os;
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -4,14 +4,19 @@
 class Fixed {
     public:
         Fixed();
+        Fixed(const int intValue);
+        Fixed(const float floatValue);
         Fixed(const Fixed& other);
         Fixed& operator=(const Fixed& other);
         ~Fixed();
         int getRawBits() const;
         void setRawBits(int bits);
+        float toFloat() const;
+        int toInt() const;
+        friend std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
     private:
         int _bits;
-        static const int _fractionalBits = 8;
+        static const int _numOfFractionalBits = 8;
 };
 
 #endif /* __FIXED_HPP__ */
