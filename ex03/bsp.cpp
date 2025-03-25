f4e39ffd48ad93fd6545937ff0a15df3176b15b7
punchlemon <tsukiwahigashini0410@gmail.com>
2025-03-25 15:12:22 +0900

    [FIX] ex03 the point on line is out

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -4,6 +4,6 @@ bool bsp( Point const a, Point const b, Point const c, Point const point) {
     Fixed cross1 = Point::crossProduct2D(a, b, point);
     Fixed cross2 = Point::crossProduct2D(b, c, point);
     Fixed cross3 = Point::crossProduct2D(c, a, point);
-    return (cross1 >= 0 && cross2 >= 0 && cross3 >= 0)
-        || (cross1 <= 0 && cross2 <= 0 && cross3 <= 0);
+    return (cross1 > 0 && cross2 > 0 && cross3 > 0)
+        || (cross1 < 0 && cross2 < 0 && cross3 < 0);
 }
